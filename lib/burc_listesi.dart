831fedb67f97362fdbefe7ca2dd67e1168429872
fadilbasaran <59345355+fadilbasaran@users.noreply.github.com>
2021-12-25 02:35:11 +0300

    fadil
    first commit

---
+++ lib/burc_listesi.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_burclar/burc_item.dart';
+import 'package:flutter_burclar/data/strings.dart';
+import 'package:flutter_burclar/model/burc.dart';
+
+class BurcListesi extends StatelessWidget {
+  late List<Burc> tumBurclar = [];
+
+  BurcListesi() {
+    tumBurclar = veriKaynaginiHazirla();
+
+    print(tumBurclar);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Burçlar Listesi'),
+        backgroundColor: Colors.pink,
+      ),
+      body: Center(
+          child: ListView.builder(
+        itemBuilder: (context, index) {
+          return BurcItem(
+            listelenenBurc: tumBurclar[index],
+          );
+        },
+        itemCount: tumBurclar.length,
+      )),
+    );
+  }
+
+  List<Burc> veriKaynaginiHazirla() {
+    List<Burc> gecici = [];
+
+    for (int i = 0; i < 12; i++) {
+      var burcAdi = Strings.BURC_ADLARI[i];
+      var burcTarih = Strings.BURC_TARIHLERI[i];
+      var burcDetay = Strings.BURC_GENEL_OZELLIKLERI[i];
+      //koc1.png değerleri oluşturmak Koc-->koc-->koc1.png
+      var burcKucukResim =
+          Strings.BURC_ADLARI[i].toLowerCase() + '${i + 1}.png';
+
+      var burcBuyukResim =
+          Strings.BURC_ADLARI[i].toLowerCase() + '_buyuk${i + 1}.png';
+      Burc eklencekBurc =
+          Burc(burcAdi, burcTarih, burcDetay, burcKucukResim, burcBuyukResim);
+      gecici.add(eklencekBurc);
+    }
+    return gecici;
+  }
+}
